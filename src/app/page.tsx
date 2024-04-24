4040d02be2dc282565fc3d85dd556753439da7bd
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-04-24 12:57:39 +0200

    feat: created the hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
 // Lading Page
 // Here is where the user will land when they visit the site
+
+import { libreBaskerville } from "@/utils/fonts";
+
 export default function Home() {
 	return (
 		<>
-			<section>
-				<h1 className="">
-					Protecting your documents, empowering your collaboration.
-				</h1>
-				<p>
-					With our state-of-the-art encryption, your documents stay
-					safe while our intuitive platform ensures hassle-free
-					sharing for effective collaboration.
-				</p>
+			<section className="flex justify-center mx-auto 2xl:max-w-7xl xl:max-w-6xl lg:max-w-4xl md:max-w-3xl xs:max-w-md">
+				<div className="flex flex-col items-center">
+					<h1
+						className={`${libreBaskerville.className} text-center max-w-6xl text-4xl font-normal mb-10 lg:text-6xl`}
+					>
+						Protecting your documents, empowering your
+						collaboration.
+					</h1>
+					<p className="max-w-2xl mx-auto text-lg ">
+						With our state-of-the-art encryption, your documents
+						stay safe while our intuitive platform ensures
+						hassle-free sharing for effective collaboration.
+					</p>
+				</div>
 			</section>
 		</>
 	);

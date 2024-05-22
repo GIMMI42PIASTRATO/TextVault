419c208a81c4389b865e8d2a06b8d7d573cdf52f
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-05-22 23:32:34 +0200

    refactor(dashboard): update NavButton to NavLink

--- next_app/src/app/dashboard/components/NavLink.tsx
+++ next_app/src/app/dashboard/components/NavLink.tsx
@@ -1,21 +1,21 @@
-import React from "react";
+import Link from "next/link";
 
 interface NavButtonProps {
 	icon: React.ReactNode;
+	href: string;
 	text: string;
-	onClick: () => void;
 }
 
-export default function NavButton({ icon, text, onClick }: NavButtonProps) {
+export default function NavButton({ icon, text, href }: NavButtonProps) {
 	return (
 		<div className="flex">
-			<button
+			<Link
+				href={href}
 				className={`flex items-center space-x-2 px-3 py-2 rounded-lg w-full gap-2 text-left hover:bg-[--dash-white-bg-hover2] dark:hover:bg-[--dash-dark-bg-hover2] dark:text-[--dash-dark-text1] cursor-pointer transition-colors duration-200 ease-in-out`}
-				onClick={onClick}
 			>
 				{icon}
 				<span className="text-sm font-medium">{text}</span>
-			</button>
+			</Link>
 		</div>
 	);
 }
--- next_app/src/app/dashboard/components/Navbar.tsx
+++ next_app/src/app/dashboard/components/Navbar.tsx
@@ -1,16 +1,12 @@
 import ShowUser from "./ShowUser";
-import NavButton from "./NavButton";
+import NavButton from "./NavLink";
 
 export default function Navbar() {
 	return (
 		<div className="flex flex-col h-screen max-w-72 box-border p-3 gap-5 bg-[--dash-white-bg2] dark:bg-[--dash-dark-bg2]">
 			<ShowUser />
 			<div>
-				<NavButton
-					icon="🏠"
-					text="Home"
-					onClick={() => console.log("ciao")}
-				/>
+				<NavButton icon="🏠" href="/dashboard" text="Home" />
 			</div>
 		</div>
 	);

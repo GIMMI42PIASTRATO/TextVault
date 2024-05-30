a6735ef3e2ff1d405fb560b890250577c7133081
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-05-30 15:59:34 +0200

    feat(NewDocBtn): Create new document on button click

--- next_app/src/app/dashboard/components/NewDocBtn.tsx
+++ next_app/src/app/dashboard/components/NewDocBtn.tsx
@@ -1,14 +1,31 @@
-"use client";
-
 import NewFileIco from "@/components/icons/NewFileIco";
 import { useUserContext } from "../context/UserContext";
+import { createBrowserClient } from "@/lib/pocketbase";
 
 export default function NewDocBtn() {
-	const handleClick = () => {};
+	const user = useUserContext();
+
+	const handleClick = async () => {
+		try {
+			const pb = createBrowserClient();
+
+			const newDocument = {
+				id_user: [user?.id],
+				title: "Untitled",
+				content: "",
+				folder: "root",
+			};
+
+			const docsCollection = await pb.collection("docs");
+			const document = await docsCollection.create(newDocument);
+		} catch (error) {
+			console.error("Error creating new document", error);
+		}
+	};
 
 	return (
 		<div onClick={handleClick}>
-			<button className="flex items-center gap-2 px-3 py-2 rounded w-full text-left bg-[#4dc869] text-[--dash-white-text1] dark:text-[--dash-dark-text1:] cursor-pointer transition-colors duration-200 ease-in-out">
+			<button className="flex items-center gap-2 px-3 py-2 rounded w-full text-left bg-[#4dc869] hover:bg-green-600 text-[--dash-dark-text1] dark:text-[--dash-white-text1] cursor-pointer transition-colors duration-200 ease-in-out">
 				<NewFileIco />
 				<span className="text-sm font-semibold">New Document</span>
 			</button>

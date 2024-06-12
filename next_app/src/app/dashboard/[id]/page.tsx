6055073b67beae2757d7e5aaf32d6fe1e2308482
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-06-12 16:30:17 +0200

    feat(editor): added blocknote rich text editor

--- next_app/src/app/dashboard/[id]/page.tsx
+++ next_app/src/app/dashboard/[id]/page.tsx
@@ -4,6 +4,12 @@ import DashboardContainer from "../components/DashboardContainer";
 import { useDocsContext } from "@/contexts/DocumentContext";
 import DocumentNotFound from "./components/DocumentNotFound";
 
+// blocknote imports
+import "@blocknote/core/fonts/inter.css";
+import { useCreateBlockNote } from "@blocknote/react";
+import { BlockNoteView } from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+
 interface DocumentEditorProps {
 	params: {
 		id: string;
@@ -13,6 +19,9 @@ interface DocumentEditorProps {
 export default function DocumentEditor({ params }: DocumentEditorProps) {
 	const { getDocsById } = useDocsContext();
 	const document = getDocsById(params.id);
+
+	// Creates a new editor instance
+	const editor = useCreateBlockNote();
 
 	if (!document) {
 		return (
@@ -25,8 +34,7 @@ export default function DocumentEditor({ params }: DocumentEditorProps) {
 	return (
 		<DashboardContainer>
 			<h1>{document.title}</h1>
-			<p>Editing document: {params.id}</p>
-			<p>{document.content}</p>
+			<BlockNoteView editor={editor} defaultValue={document.content} />
 		</DashboardContainer>
 	);
 }

eb632a914db6928f4cf7adb7abe237dafecede98
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-06-28 18:00:10 +0200

    style(DocumentNotFound): update document not found message and styling

--- next_app/src/app/dashboard/[id]/components/DocumentNotFound.tsx
+++ next_app/src/app/dashboard/[id]/components/DocumentNotFound.tsx
@@ -1,10 +1,13 @@
-import { TbFileSad } from "react-icons/tb";
+import { FileX2 } from "lucide-react";
 
 export default function DocumentNotFound() {
 	return (
-		<div className="flex flex-col justify-center items-center">
-			<TbFileSad className="w-24 h-24 text-gray-400 dark:text-gray-500" />
-			<h1 className="text-2xl font-semibold mt-2">Document not found</h1>
+		<div className="flex flex-col justify-center items-center h-96">
+			<FileX2 size={48} strokeWidth={1} />
+			<h3 className="text-sm font-medium mt-2">No document found</h3>
+			<p className="mt-1 text-sm text-[--dash-dark-text2]">
+				Please check the document ID and try again.
+			</p>
 		</div>
 	);
 }

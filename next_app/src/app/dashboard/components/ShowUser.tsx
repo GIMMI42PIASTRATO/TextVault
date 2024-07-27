face5da43adf2fe98c0063387e93fc6e902e5691
GIMMI42PIASTRATO <67071988+GIMMI42PIASTRATO@users.noreply.github.com>
2024-07-27 17:02:19 +0200

    feat(UserDropDown): create UserDropDown component

--- next_app/src/app/dashboard/components/ShowUser.tsx
+++ next_app/src/app/dashboard/components/ShowUser.tsx
@@ -2,9 +2,7 @@ import { createAvatar } from "@dicebear/core";
 import { loreleiNeutral } from "@dicebear/collection";
 import { useUserContext } from "../context/UserContext";
 import { Skeleton } from "@/components/ui/skeleton";
-
-// components
-import Settings from "./Settings";
+import UserDropDown from "./UserDropDown";
 
 export default function ShowUser() {
 	const user = useUserContext();
@@ -18,37 +16,38 @@ export default function ShowUser() {
 	}).toString();
 
 	return (
-		<div className="grid grid-cols-[1fr_6rem_1fr] gap-3 items-center cursor-pointer transition-colors duration-200 ease-in-out hover:bg-[--dash-white-bg-hover2] dark:hover:bg-[--dash-dark-bg-hover2] px-2 py-2 rounded-md overflow-hidden">
-			{user ? (
-				<div dangerouslySetInnerHTML={{ __html: svgAvatar }}></div>
-			) : (
-				// if user is not available, dysplay the skeleton loader
-				// add a color for the dark mode
-				<Skeleton className="w-12 h-12 rounded-full bg-[--dash-white-accent1] dark:bg-[--dash-dark-accent1]" />
-			)}
-			<div className="w-full flex flex-col">
+		<UserDropDown user={user}>
+			<div className="grid grid-cols-[1fr_9.8rem] gap-3 items-center cursor-pointer transition-colors duration-200 ease-in-out hover:bg-[--dash-white-bg-hover2] dark:hover:bg-[--dash-dark-bg-hover2] px-2 py-2 rounded-md overflow-hidden">
 				{user ? (
-					<span className="text-sm font-semibold overflow-hidden text-ellipsis text-nowrap">
-						{user?.name}
-					</span>
+					<div dangerouslySetInnerHTML={{ __html: svgAvatar }}></div>
 				) : (
-					<Skeleton className="w-24 h-4 rounded-lg bg-[--dash-white-accent1] dark:bg-[--dash-dark-accent1] mb-2" />
-					// <span className="text-center mb-1 rounded-lg text-sm font-semibold overflow-hidden text-ellipsis bg-gray-200 animate-pulse dark:bg-zinc-700">
-					// 	...
-					// </span>
+					// if user is not available, dysplay the skeleton loader
+					// add a color for the dark mode
+					<Skeleton className="w-12 h-12 rounded-full bg-[--dash-white-accent1] dark:bg-[--dash-dark-accent1]" />
 				)}
-				{user ? (
-					<span className="text-xs text-gray-500 overflow-hidden text-ellipsis text-nowrap">
-						{user?.email}
-					</span>
-				) : (
-					<Skeleton className="w-24 h-3 rounded-lg bg-[--dash-white-accent1] dark:bg-[--dash-dark-accent1]" />
-					// <span className="text-xs text-center rounded-lg bg-gray-200 animate-pulse dark:bg-zinc-700">
-					// 	...
-					// </span>
-				)}
+				<div className="w-full flex flex-col">
+					{user ? (
+						<span className="text-sm font-semibold overflow-hidden text-ellipsis text-nowrap">
+							{user?.name}
+						</span>
+					) : (
+						<Skeleton className="w-24 h-4 rounded-lg bg-[--dash-white-accent1] dark:bg-[--dash-dark-accent1] mb-2" />
+						// <span className="text-center mb-1 rounded-lg text-sm font-semibold overflow-hidden text-ellipsis bg-gray-200 animate-pulse dark:bg-zinc-700">
+						// 	...
+						// </span>
+					)}
+					{user ? (
+						<span className="text-xs text-gray-500 overflow-hidden text-ellipsis text-nowrap">
+							{user?.email}
+						</span>
+					) : (
+						<Skeleton className="w-24 h-3 rounded-lg bg-[--dash-white-accent1] dark:bg-[--dash-dark-accent1]" />
+						// <span className="text-xs text-center rounded-lg bg-gray-200 animate-pulse dark:bg-zinc-700">
+						// 	...
+						// </span>
+					)}
+				</div>
 			</div>
-			<Settings />
-		</div>
+		</UserDropDown>
 	);
 }
---
+++ next_app/src/app/dashboard/components/UserDropDown.tsx
@@ -0,0 +1,99 @@
+import { useState } from "react";
+
+import { UsersResponse } from "@/types/pocketbase-types";
+
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuRadioGroup,
+	DropdownMenuSub,
+	DropdownMenuSubTrigger,
+	DropdownMenuPortal,
+	DropdownMenuSubContent,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+	DropdownMenuRadioItem,
+} from "@/components/ui/dropdown-menu";
+
+import { Palette, Sun, Moon, Monitor, Cog, LogOut } from "lucide-react";
+
+type UserDropDownProps = {
+	children: React.ReactNode;
+	user: UsersResponse | null;
+};
+
+export default function UserDropDown({ children, user }: UserDropDownProps) {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+			<DropdownMenuContent
+				className="dark:bg-[--dash-dark-bg1] dark:border-[--dash-dark-accent1] w-56"
+				align="center"
+			>
+				<DropdownMenuLabel className="font-normal">
+					<p className="text-s font-semibold leading-none text-muted-foreground">
+						{user?.email}
+					</p>
+				</DropdownMenuLabel>
+				<DropdownMenuSeparator className="dark:bg-[--dash-dark-accent1]" />
+				<DropdownMenuGroup>
+					<ModeToggle />
+					<DropdownMenuItem className="dark:hover:bg-[--dash-dark-selected-hover]">
+						<Cog className="mr-2 h-4 w-4" />
+						<span>Settings</span>
+					</DropdownMenuItem>
+					<DropdownMenuItem className="dark:hover:bg-[--dash-dark-selected-hover]">
+						<LogOut className="mr-2 h-4 w-4" />
+						<span>Logout</span>
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+}
+
+export function ModeToggle() {
+	const [position, setPosition] = useState<string>("system");
+
+	return (
+		<DropdownMenuSub>
+			<DropdownMenuSubTrigger className="dark:data-[state=open]:bg-[--dash-dark-accent2] dark:hover:bg-[--dash-dark-accent2]">
+				<Palette className="mr-2 h-4 w-4" />
+				<span>Theme</span>
+			</DropdownMenuSubTrigger>
+			<DropdownMenuPortal>
+				<DropdownMenuSubContent className="w-[180px] dark:bg-[--dash-dark-bg1] dark:border-[--dash-dark-accent1]">
+					<DropdownMenuRadioGroup
+						value={position}
+						onValueChange={setPosition}
+					>
+						<DropdownMenuRadioItem
+							value="light"
+							className="dark:hover:bg-[--dash-dark-selected-hover]"
+						>
+							<Sun className="mr-2 h-4 w-4" />
+							<span>Light</span>
+						</DropdownMenuRadioItem>
+						<DropdownMenuRadioItem
+							value="dark"
+							className="dark:hover:bg-[--dash-dark-selected-hover]"
+						>
+							<Moon className="mr-2 h-4 w-4" />
+							<span>Dark</span>
+						</DropdownMenuRadioItem>
+						<DropdownMenuRadioItem
+							value="system"
+							className="dark:hover:bg-[--dash-dark-selected-hover]"
+						>
+							<Monitor className="mr-2 h-4 w-4" />
+							<span>System</span>
+						</DropdownMenuRadioItem>
+					</DropdownMenuRadioGroup>
+				</DropdownMenuSubContent>
+			</DropdownMenuPortal>
+		</DropdownMenuSub>
+	);
+}
